b16b9437afc6622745ae426bd47d5f92389c1fd2
puneku <puneetkumar2019@gmail.com>
2022-12-10 23:00:48 +0530

    [ADD]-added pagination and error handling

--- youtube_data/views.py
+++ youtube_data/views.py
@@ -4,6 +4,8 @@ from youtube_data.serializers import YoutubeVideosSerializer
 from .models import YoutubeVideos
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from django.core.paginator import Paginator
 
 def home(request):    
     return HttpResponse("<div style='text-align:center'><h1>Hello, Fampay!</h1></div>")
@@ -15,12 +17,18 @@ def home(request):
 class Videos(APIView):
 
     def get(self, request):
-        videos_list = YoutubeVideos.objects.all()
-        serializer = YoutubeVideosSerializer(videos_list, many=True)
+        page = request.GET.get('page', 1)
+        per_page_count = request.GET.get('per_page_count', 5)
+        videos_list = YoutubeVideos.objects.all().order_by('-published_date')
+        paginator = Paginator(videos_list, per_page_count)
+        curr_page_data = paginator.get_page(page) 
+        serializer = YoutubeVideosSerializer(curr_page_data, many=True)
         return Response(serializer.data)
 
     def post(self, request):
-        serializer = YoutubeVideosSerializer(data=request.data)
+        data = request.data
+        serializer = YoutubeVideosSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-        return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
